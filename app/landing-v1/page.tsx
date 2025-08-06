10cc97d922b5753428f5c9f49115da398a11ebdb
jorge-arean-dev <jorge.arean.dev@gmail.com>
2025-08-06 16:31:31 -0300

    added videobackground in 4k

--- app/landing-v1/page.tsx
+++ app/landing-v1/page.tsx
@@ -4,17 +4,31 @@ import LandingFooter from "@/components/landing-v1/landing-footer"
 
 export default function LandingPageV1() {
   return (
-    <main 
-      className="min-h-screen flex flex-col bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('/background/back2.jpeg')" }}
-    >
-      <LandingHeader />
-      <div className="flex-1 flex items-center justify-center">
-        <div className="max-w-5xl mx-auto px-5 w-full">
-          <LandingHero />
+    <main className="relative min-h-screen flex flex-col">
+      {/* Fullscreen Video Background */}
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute inset-0 w-full h-full object-cover -z-10"
+      >
+        <source 
+          src="https://res.cloudinary.com/dnhjrpwmc/video/upload/v1754507917/background-2-4k_nkezn5.mp4" 
+          type="video/mp4" 
+        />
+      </video>
+      
+      {/* Content Overlay */}
+      <div className="relative z-10 min-h-screen flex flex-col">
+        <LandingHeader />
+        <div className="flex-1 flex items-center justify-center">
+          <div className="max-w-5xl mx-auto px-5 w-full">
+            <LandingHero />
+          </div>
         </div>
+        <LandingFooter />
       </div>
-      <LandingFooter />
     </main>
   )
 }
